1a152776cd1e4d859fe5dae50bc3914cac9c2c54
Nadav <nadav.matalon@gmail.com>
2014-11-11 22:54:59 +0000

    working on front-end testing

--- test/features/ticTacToeTest.js
+++ test/features/ticTacToeTest.js
@@ -14,7 +14,7 @@ describe('Tic-Tac-Toe', function() {
 		browser = Browser.create({ site: 'http://localhost:3000', debug: true });
 	});
 
-	describe('page features', function() {
+	describe('page', function() {
 
 		before('refresh page', function(done) {
 			browser.visit('http://localhost:3000/', done);
@@ -31,12 +31,33 @@ describe('Tic-Tac-Toe', function() {
 		it('should contain the correct page heading', function() {
 			expect(browser.text('#page-title')).to.equal('TIC • TAC • TOE');
 		});
+	});	
+
+ 	describe('grid', function() {
+
+		beforeEach('refresh page', function(done) {
+			browser.visit('http://localhost:3000/', done);
+		});
 
 		it('should contain 9 squares', function() {
 			var squareArray = browser.queryAll('.square');
 			expect(squareArray).to.have.length.of(9);
 		});
-	});	
+
+		it('squares should initially be empty', function() {
+			['.button-one', '.button-two', '.button-three', '.button-four',
+			 '.button-five', '.button-six', '.button-seven', '.button-eight',
+			 '.button-nine'].forEach(function(square) {
+				expect(browser.query(square).value).to.equal('');
+			});
+        });
+
+		it('square should be assigned a value when clicked', function() {
+			var currentTurn = browser.evaluate("game.currentTurn");
+			browser.pressButton('#square-one');
+			expect(browser.query('.button-one').value).to.equal(currentTurn);
+    	});
+	});
 
  	describe('game', function() {
 
@@ -48,59 +69,23 @@ describe('Tic-Tac-Toe', function() {
             expect(browser.evaluate("game.gameOver()")).to.be.false;
         });
 
-		it('squares should all be initially empty', function() {
-            expect(browser.query('.button-one').value).to.equal('');
-            expect(browser.query('.button-two').value).to.equal('');
-            expect(browser.query('.button-three').value).to.equal('');
-            expect(browser.query('.button-four').value).to.equal('');
-            expect(browser.query('.button-five').value).to.equal('');
-            expect(browser.query('.button-six').value).to.equal('');
-            expect(browser.query('.button-seven').value).to.equal('');
-            expect(browser.query('.button-eight').value).to.equal('');
-            expect(browser.query('.button-nine').value).to.equal('');
-        });
-
-		it('square should get a value if clicked', function() {
-			var currentTurn = browser.evaluate("game.currentTurn");
-			browser.pressButton('#square-one');
-			expect(browser.query('.button-one').value).to.equal(currentTurn);
-    	});
-
 		it('should be over if winning sequence is reached', function() {
-			var squares = [
-				'#square-one', 
-				'#square-two', 
-				'#square-three',
-				'#square-four',
-				'#square-five',
-				'#square-six',
-				'#square-seven'
-				]
-			squares.forEach(function(square) {
+			['#square-one', '#square-two', '#square-three', 
+			 '#square-four', '#square-five', '#square-six', 
+			 '#square-seven'].forEach(function(square) {
 				browser.pressButton(square);
 			});
 			expect(browser.evaluate("game.gameOver()")).to.be.true;
 		});
 
 		it('should be over if all possible moves were made', function() {
-			var squares = [
-				'#square-one', 
-				'#square-two', 
-				'#square-three',
-				'#square-four',
-				'#square-six',
-				'#square-nine',
-				'#square-five',
-				'#square-seven',
-				'#square-eight'
-				]
-			squares.forEach(function(square) {
+			['#square-one', '#square-two', '#square-three', '#square-four',
+			 '#square-six', '#square-nine', '#square-five', '#square-seven', 
+			 '#square-eight'].forEach(function(square) {
 				browser.pressButton(square);
 			});
 			expect(browser.evaluate("game.gameOver()")).to.be.true;
 		});
-
-
  	});
 });
 
