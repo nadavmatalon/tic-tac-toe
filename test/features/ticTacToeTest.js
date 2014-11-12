cd022e925eda672e84ed6e6e7f575f9202953aab
Nadav <nadav.matalon@gmail.com>
2014-11-12 01:49:57 +0000

    working on front-end testing

--- test/features/ticTacToeTest.js
+++ test/features/ticTacToeTest.js
@@ -8,15 +8,15 @@ var Browser = require('zombie');
 var browser;
 
 var squareValues = [
-	'.input-0', 
-	'.input-1', 
-	'.input-2', 
-	'.input-3', 
+	'.input-0',
+	'.input-1',
+	'.input-2',
+	'.input-3',
 	'.input-4',
-    '.input-5', 
-    '.input-6', 
-    '.input-7', 
-    '.input-8'
+	'.input-5', 
+	'.input-6',
+	'.input-7',
+	'.input-8'
 ];
 
 describe('Tic-Tac-Toe', function() {
