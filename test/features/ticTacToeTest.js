e1282755669dbc2b9212b785c0c9f5ec3c316a86
Nadav <nadav.matalon@gmail.com>
2014-11-12 01:35:06 +0000

    working on front-end testing

--- test/features/ticTacToeTest.js
+++ test/features/ticTacToeTest.js
@@ -10,9 +10,6 @@ var browser;
 var squareValues = ['.input-0', '.input-1', '.input-2', '.input-3', '.input-4', 
 					'.input-5', '.input-6', '.input-7', '.input-8'];
 
-
-
-
 describe('Tic-Tac-Toe', function() {
 
 	before('run server', function() {
